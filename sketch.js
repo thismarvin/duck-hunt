184f8aad6dc98dd0af7150441fb789db3a4ec241
austin0209 <austinrandy0209@gmail.com>
2018-09-23 20:21:45 -0500

    more important comments

--- sketch.js
+++ sketch.js
@@ -6,6 +6,11 @@ function setup() {
     //prompt("SUP MARVIN");
     // what up
     // my name is austin
+    //line 1
+    //line 2
+    //line 4
+    alert("DSFSF");
+    //line 2342
 }
 
 function draw() {
