2658196c764cd910205ce1c35a382a284ecc6207
Marvin Valenzuela <bend_@outlook.com>
2018-09-23 21:48:26 -0500

    ready to start

--- sketch.js
+++ sketch.js
@@ -3,21 +3,10 @@ const screenH = 400;
 
 function setup() {
     createCanvas(screenW, screenH);
-    //prompt("SUP MARVIN");
-    // what up
-    // my name is austin
-    //line 1
-    //line 2
-    //line 4
-    alert("DSFSF");
-    //line 2342
-    let rect = new Rectangle(0, 0, 40, 40);
-}
-
-function update() {
 
 }
 
+// This is update
 function draw() {
     update();
     background(0);
