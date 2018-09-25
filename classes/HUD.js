46841e6e8d310af7cfaf0ab10b6279387ed45fc0
Marvin Valenzuela <bend_@outlook.com>
2018-09-25 18:21:23 -0500

    more work on HUD

--- classes/HUD.js
+++ classes/HUD.js
@@ -54,20 +54,23 @@ class HUD {
     }
 
     update() {
-
+        this.show();
     }
 
-    draw() {
+    show() {
         for (let i = 0; i < this.progress.length; i++) {
             switch (this.progress[i]) {
                 case "HIT":
-
+                    fill(255, 0, 77);
+                    rect(i * 8, 8, 7, 7);
                     break;
                 case "MISS":
-
+                    fill(255);
+                    rect(i * 8, 8, 7, 7);
                     break;
                 case " ":
-
+                    fill(255);
+                    rect(i * 8, 8, 7, 7);
                     break;
             }
         }
