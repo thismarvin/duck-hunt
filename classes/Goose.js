960fb4191f17bf13e9aa4a6612622d9b2f1aabaf
austin0209 <austinrandy0209@gmail.com>
2018-09-26 23:06:31 -0500

    did move method

--- classes/Goose.js
+++ classes/Goose.js
@@ -11,22 +11,43 @@ class Goose extends Entity {
     this.deltaX = Math.random() + 0.2 * speed; // change later, make min dynamic.
     this.deltaY = Math.random() + 0.2 * speed; // change later, make min dynamic.
 
-    this.fallen = false; // whats the difference?
-    this.fall = false;
-
-    this.dead = false;
+    this.hasFallen = false; // whats the difference?
+    this.shouldFall = false;
+    this.shouldFlyAway = false;
   }
 
   update() {
     this.show();
-    // Temporary, for debugging
-    if (this.fall) {
-      super.move(0, 1);
-    }
+    this.move();
   }
 
   show() {
     fill(255);
     super.show();
+  }
+
+  move() {
+    if (this.shouldFall) {
+      super.move(0, 1);
+    } else if (this.shouldFlyAway) {
+      super.move(0, -1);
+      if (this.y < 0) {
+        this.dead = true;
+      }
+    } else {
+      if (this.x > screenW || x < 0) {
+        this.deltaX = -this.deltaX;
+      }
+      if (this.y > screenH || y < 0) {
+        this.deltaY = -this.deltaY;
+      }
+      super.move(this.deltaX, this.deltaY);
+    }
+  }
+
+  // this will probably be useful later.
+  invertDirection() {
+    this.deltaX = -this.deltaX;
+    this.deltaY = -this.deltaY;
   }
 }
