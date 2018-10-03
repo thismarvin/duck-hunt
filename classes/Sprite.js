ebcf2ebbd299f1d97c7b1a08a1110ed65ce8bf12
Marvin Valenzuela <bend_@outlook.com>
2018-10-02 22:48:35 -0500

    temp solution for animations

--- classes/Sprite.js
+++ classes/Sprite.js
@@ -1,15 +1,35 @@
 class Sprite {
-    constructor(x, y, sWidth, sHeight, spriteSheet, sourceX=0, sourceY=0) {
+    constructor(x, y, sWidth, sHeight, spriteSheet, sourceX = 0, sourceY = 0) {
         this.x = x;
         this.y = y;
-
         this.sWidth = sWidth;
         this.sHeight = sHeight;
-
         this.sourceX = sourceX * sWidth;
-        this.sourceY = sourceY * sWidth;
-
+        this.sourceY = sourceY * sHeight;
         this.spriteSheet = spriteSheet;
+
+        // temp
+        this.timer = new Timer(80);
+        this.frame = 0;
+    }
+
+    setLocation(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    setSpriteSheet(spriteSheet){
+        this.spriteSheet = spriteSheet;
+    }
+
+    // temp
+    update() {
+        if (this.timer.isFinished()) {
+            this.frame = this.frame + 1 > 7 ? 0 : ++this.frame;
+            this.sourceX = this.frame * this.sWidth;
+            this.timer.reset();
+        }
+
     }
 
     show() {
